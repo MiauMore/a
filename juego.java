c26b6a8355a8814b030510198a1dd84fa2140643
antoniojeje <106982226+antoniojeje@users.noreply.github.com>
2022-07-06 09:59:07 -0400

    Update juego.java
    Magic now has a purpose on the game and the seduction option has been turned into a spell

--- juego.java
+++ juego.java
@@ -2,18 +2,23 @@ import java.util.Scanner;
 
 public class juego{
 	public static void main(String[] args){
-		int hp=100 , hpe=50 , atk=10 , atke=20 ,op;
+		int hp=100 , hpe=50 , atk=10 , atke=20 ,op, oph;
 		float def=1;
 		Scanner leer= new Scanner(System.in);
 	System.out.println("Te encuentras con un enemigo");
 	System.out.println("=====================================");
 	do{
-		System.out.println("¿que haces?");
+	    System.out.println("");
+		System.out.println("Tu hp es:"+hp);
+		System.out.println("Tu ataque es:"+atk);
+		System.out.println("El hp del enemigo es de:"+hpe);
+		System.out.println("El ataque del enemigo es de:"+atke);
+		System.out.println("");
+		System.out.println("¿Que quieres hacer?");
 		System.out.println("(1) atacar");
 		System.out.println("(2) defender");
-		System.out.println("(3) seducir");
-		System.out.println("(4) esteroides");
-		System.out.println("(5) magia");
+		System.out.println("(3) Magia");
+		System.out.println("(4) Esteroides");
 		op= leer.nextInt();
 		def=1;
 		switch(op){
@@ -27,26 +32,41 @@ public class juego{
 			def= (float)0.2;
 			break;
 			case 3:
-			System.out.println("intentas seducir al enemigo");
-			System.out.println("has tenido exito, ataque se reduce");
-			atke= (int)(atke*0.9);
-			break;
+            System.out.println("Lista de hechizos:");
+            System.out.println("(1) Seduccion");
+            oph= leer.nextInt();
+            switch(oph){
+              case 1:
+                if (hp>=15){
+                System.out.println("Intentas seducir al enemigo");
+			    System.out.println("Has tenido exito, ataque se reduce");
+                System.out.println("El hechizo ha extraido parte de tu vida");
+                hp=hp-15;
+			    atke= (int)(atke-(atke*0.5));
+                }else{
+                  System.out.println("No tienes suficiente vida para realizar este hechizo");
+                }
+                break;
+              default: System.out.println("el hechizo ha fallado");
+                break;
+            }
+            break;
 			case 4:
 			System.out.println("te haces una sopa de esteroides");
 			System.out.println("tu daño sube");
-			atk= atk+10;
-			break;
-			case 5:
-			System.out.println("intentas hacer magia");
-			System.out.println("no ha pasado nada");
+			atk= atk+5;
 			break;
 			default:
 			System.out.println("no haces nada");
 		}//fin swicth
 		System.out.println("=====================================");
 		hp= (hp- (int)(atke*def));
+		if (hpe>0){
 		System.out.println("el enemigo te golpea");
 		System.out.println("recibes " + (int)(atke*def) + " de danno");
+		}else{
+		System.out.println("Has derrotado al enemigo");
+		}
 	}while(hp>0 && hpe>0);
 	System.out.println("juego terminado");
 	
