9d92cfe5cd59a473ad8ca6d575313b70728a2cca
J4un <67522165+J4un@users.noreply.github.com>
2022-09-26 11:45:59 -0300

    Add files via upload

---
+++ PruebaPoo/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Cuadrado prueba1= new Cuadrado(20);
+        Cuadrado prueba2= new Cuadrado(3);
+        System.out.println (prueba1.calculoArea()+" "+prueba2.calculoArea());
+    }
+}
